766958025dffd0134bc1dc44493b8491d0f7b95d
David Gustafson <git@radicalskeptic.net>
2014-04-29 15:15:21 -0700

    Implements testing for posting a todo.

---
+++ spec/features/projuct_manager_creates_todo_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Product manager creates TODO' do
+  scenario 'Successfully' do
+    visit new_todo_path
+    fill_in 'Desc', with: "Meet up with the team."
+    click_button 'Save'
+    #expect( page ).to have_content('Your new TODO was saved')
+    assert page.has_content?('Your new TODO was saved')
+
+    #expect( page ).to have_content('Meet up with the team')
+    assert page.has_content?('Meet up with the team')
+  end
+
+  scenario 'With description missing' do
+    visit new_todo_path
+    fill_in 'Desc', with: ""
+    click_button 'Save'
+    assert page.has_content?('Description may not be blank')
+  end
+end
