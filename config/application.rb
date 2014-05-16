d3321bf63768af820908d5be7495fc13b14bf749
David Gustafson <git@radicalskeptic.net>
2014-05-16 13:34:52 -0700

    added some commented code a nonfunctional checkbox and a style for validation errors

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ Bundler.require(*Rails.groups)
 
 module Blocitoff
   class Application < Rails::Application
+    #config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      #"<div class=\"alert-danger\">#{html_tag}</div>".html_safe
+    #}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
